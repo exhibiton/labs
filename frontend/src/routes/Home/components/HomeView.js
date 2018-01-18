41aa751143c24b5a0e6b294fef6b22ccff211d11
Shawn Vo <shawn.vo@gmail.com>
2018-01-18 17:08:22 -0500

    created home page layout with bootstrap

--- frontend/src/routes/Home/components/HomeView.js
+++ frontend/src/routes/Home/components/HomeView.js
@@ -1,34 +1,29 @@
 import React from 'react'
-import bg from '../assets/placeholder.png'
+import LabsLogo from '../assets/WeWorkLabsLogo.png'
 import './HomeViewStyle.scss'
 
+import LoginForm from './LoginForm'
+
 export const HomeView = () => (
-  <div className='bg'>
-    <br />
-    <br />
-    <br />    
-    <br />
-    <br />
-    <br />    
-    <br />
-    <br />
-    <br />    
-    <br />
-    <br />
-    <br />    
-    <br />
-    <br />
-    <br />    
-    <br />
-    <br />
-    <br />
-    <br />    
-    <br />
-    <br />
-    <br />    
-    <br />
-    <br />
-    <br />
+  <div>
+    <div className='row'>
+      <div className='col-md-7 bg d-none d-md-block'>
+      </div>
+      <div className='col-md-5 col-sm-12'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <img src={LabsLogo} alt={"WeWork Labs Logo"} className='labs-logo'/>
+          </div>
+        </div>
+        <div className='row justify-content-md-center'>
+          <div className='col-md-8'>
+            <h2 className='text-center'>Hello</h2>
+            <p className='text-center'>Some text about wework labs and how dope and exclusive it is.</p>
+            <LoginForm/>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
