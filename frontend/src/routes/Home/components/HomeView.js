7c2b8961abcde7c94fa608afdab95daba9e60d92
Shawn Vo <shawn.vo@gmail.com>
2018-01-19 10:10:23 -0500

    changed bootstrap to flexbox

--- frontend/src/routes/Home/components/HomeView.js
+++ frontend/src/routes/Home/components/HomeView.js
@@ -6,21 +6,15 @@ import LoginForm from './LoginForm'
 
 export const HomeView = () => (
   <div>
-    <div className='row'>
-      <div className='col-md-7 bg d-none d-md-block'>
+    <div className='flex-row'>
+      <div className='bg'>
       </div>
-      <div className='col-md-5 col-sm-12'>
-        <div className='row'>
-          <div className='col-md-12'>
-            <img src={LabsLogo} alt={"WeWork Labs Logo"} className='labs-logo'/>
-          </div>
-        </div>
-        <div className='row justify-content-md-center'>
-          <div className='col-md-8'>
+      <div className='flex-column'>
+        <img src={LabsLogo} alt={"WeWork Labs Logo"} className='labs-logo'/>
+        <div className='mal side-container'>
             <h2 className='text-center'>Hello</h2>
             <p className='text-center'>Some text about wework labs and how dope and exclusive it is.</p>
             <LoginForm/>
-          </div>
         </div>
       </div>
     </div>
