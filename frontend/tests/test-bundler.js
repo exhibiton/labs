9d231850179bc7075cba1e9621c7987a9fc8ba5a
Jesse Heikkila <jesseheikkila@Jesses-MacBook-Pro.local>
2018-01-22 13:30:16 -0500

    fix tests finally, remove legacy ones

--- frontend/tests/test-bundler.js
+++ frontend/tests/test-bundler.js
@@ -5,7 +5,10 @@ import dirtyChai from 'dirty-chai'
 import chaiAsPromised from 'chai-as-promised'
 import sinonChai from 'sinon-chai'
 import chaiEnzyme from 'chai-enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
 
+configure({ adapter: new Adapter() });
 // Mocha / Chai
 // ------------------------------------
 mocha.setup({ ui: 'bdd' })
