11990cd0acbd67be8623b4b32498de4a600f9e13
Jesse Heikkila <jesseheikkila@Jesses-MacBook-Pro.local>
2018-01-22 20:13:37 -0500

    new default image

--- backend/app/models/company.rb
+++ backend/app/models/company.rb
@@ -6,7 +6,7 @@ class Company < ApplicationRecord
   # validations
   validates_presence_of :name, :description
 
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://cdn.playven.com/weworklabs.png"
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
 
 end
