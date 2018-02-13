4e8f326992cca88ab748424c830cd97724c80e68
anatoliykot <emleninovanatoliy@gmail.com>
2018-02-13 10:27:36 +0700

    Fix search technologies

--- frontend/src/actions/technology-actions.js
+++ frontend/src/actions/technology-actions.js
@@ -4,6 +4,9 @@ export const CREATE_TECHNOLOGY_LOADING = 'CREATE_TECHNOLOGY_LOADING'
 export const TECHNOLOGIES_LOADING = 'TECHNOLOGIES_LOADING'
 export const TECHNOLOGIES_LOADING_FAIL = 'TECHNOLOGIES_LOADING_FAIL'
 export const TECHNOLOGIES_LOADING_SUCCESS = 'TECHNOLOGIES_LOADING_SUCCESS'
+export const GET_TECHNOLOGIES_BY_TERM_LOADING = 'GET_TECHNOLOGIES_BY_TERM_LOADING'
+export const GET_TECHNOLOGIES_BY_TERM_FAIL = 'GET_TECHNOLOGIES_BY_TERM_FAIL'
+export const GET_TECHNOLOGIES_BY_TERM_SUCCESS = 'GET_TECHNOLOGIES_BY_TERM_SUCCESS'
 
 export const createTechnologySuccess = promise => ({
   type: CREATE_TECHNOLOGY_SUCCESS,
@@ -23,10 +26,25 @@ export const technologiesLoadingSuccess = payload => ({
   payload,
 })
 
-export const technologiesLoadingFail = () => ({
+export const technologiesLoadingFail = payload => ({
   type: TECHNOLOGIES_LOADING_FAIL,
+  payload,
 })
 
 export const technologiesLoading = () => ({
   type: TECHNOLOGIES_LOADING,
+})
+
+export const getTechnologiesByTermLoading = () => ({
+  type: GET_TECHNOLOGIES_BY_TERM_LOADING,
+})
+
+export const getTechnologiesByTermFail = payload => ({
+  type: GET_TECHNOLOGIES_BY_TERM_FAIL,
+  payload,
+})
+
+export const getTechnologiesByTermSuccess = payload => ({
+  type: GET_TECHNOLOGIES_BY_TERM_SUCCESS,
+  payload,
 })
